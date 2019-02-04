43f34174d70e3229bf3af2f6de221e7db33d4179
Steven <>
2019-02-04 14:25:18 -0500

    disc bouncing, sword sticking

---
+++ VrForcePowers/Assets/_scripts/Disc.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disc : MonoBehaviour
+{
+    public float speed;
+    public int bounces, maxBounces;
+    public Transform returnSpot;
+    public GameObject lastCollision;
+        public bool canBounce;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bounces >= maxBounces)
+        {
+            if (Vector3.Distance(transform.position, returnSpot.position) > 1)
+            {
+                rb.velocity = (returnSpot.position - transform.position).normalized * speed;
+
+            }
+                else {
+                    canBounce = false;
+                    bounces = 0;
+                }
+        }
+    }
+    public void Thrown()
+    {
+        lastCollision = null;
+        bounces = 0;
+        canBounce = true;
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (canBounce == true )
+        {
+            if (bounces < maxBounces)
+            {
+                if (lastCollision != collision.gameObject)
+                {
+                    lastCollision = collision.gameObject;
+                    RaycastHit hit;
+
+                    Physics.Raycast(transform.position, transform.right, out hit, 100f);
+
+                    bounces++;
+                    float velMag = rb.velocity.magnitude;
+                    rb.velocity = Vector3.zero;
+                    rb.angularVelocity = Vector3.zero;
+                    rb.velocity = (hit.point - transform.position).normalized * velMag * 2;
+                  
+                }
+            }
+            else
+            {
+                if (lastCollision != collision.gameObject)
+                {
+                    canBounce = false; bounces = 0;
+                }
+                    float velMag = rb.velocity.magnitude;
+                rb.velocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+                rb.velocity = (returnSpot.position - transform.position) * speed;
+            }       
+        }
+    }
+}
---
+++ VrForcePowers/Assets/_scripts/Sword.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    public Sword handle,blade;
+    public bool canStick,stuckInWall,isBlade;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Grabbed()
+    {
+        blade.canStick = true;
+        blade.stuckInWall = false;
+        GetComponent<Rigidbody>().isKinematic = false;
+    }
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        if (stuckInWall == false && canStick == true && isBlade == true)
+        {
+
+            stuckInWall = true;
+            canStick = false;
+            handle.GetComponent<Rigidbody>().isKinematic = true;
+
+        }
+    }
+}
